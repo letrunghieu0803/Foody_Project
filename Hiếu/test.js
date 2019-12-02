f24ca39d5f8409ab1631c40db59c531081b92b56
HieuLT <letrunghieu08031998@gmail.com>
2019-12-03 02:11:01 +0700

    Hieu update

---
+++ Hiếu/test.js
@@ -0,0 +1,6 @@
+const searchInput = document.getElementById("search-input")
+const searchButton = document.getElementById("search-button")
+
+searchButton.addEventListener("click", () => {
+    window.open("file:///C:/Users/Admin/Desktop/Baitap/project/post.html")
+})
